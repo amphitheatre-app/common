78fb67ff8d30496477d294b77f0ef437d52909e8
Eguo Wang <wangeguo@gmail.com>
2023-08-16 22:27:15 +0800

    Add unit tests for devcontainer::types

--- src/devcontainer/types.rs
+++ src/devcontainer/types.rs
@@ -14,21 +14,90 @@
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
 pub enum IntegerOrString {
     Integer(i32),
     String(String),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
 pub enum StringOrArray {
     String(String),
     Array(Vec<String>),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
 pub enum IntegerOrStringOrArray {
     Integer(u32),
     String(String),
     Array(Vec<IntegerOrString>),
+}
+
+#[cfg(test)]
+mod test {
+    use super::IntegerOrString;
+    use super::IntegerOrStringOrArray;
+    use super::StringOrArray;
+
+    #[test]
+    fn test_integer_or_string() {
+        use serde_json::json;
+
+        let json = json!(42);
+        let integer: IntegerOrString = serde_json::from_value(json).unwrap();
+        assert_eq!(integer, IntegerOrString::Integer(42));
+
+        let json = json!("42");
+        let string: IntegerOrString = serde_json::from_value(json).unwrap();
+        assert_eq!(string, IntegerOrString::String("42".to_string()));
+    }
+
+    #[test]
+    fn test_string_or_array() {
+        use serde_json::json;
+
+        let json = json!("42");
+        let string: StringOrArray = serde_json::from_value(json).unwrap();
+        assert_eq!(string, StringOrArray::String("42".to_string()));
+
+        let json = json!(["42", "43"]);
+        let array: StringOrArray = serde_json::from_value(json).unwrap();
+        assert_eq!(
+            array,
+            StringOrArray::Array(vec!["42".to_string(), "43".to_string()])
+        );
+    }
+
+    #[test]
+    fn test_integer_or_string_or_array() {
+        use serde_json::json;
+
+        let json = json!(42);
+        let integer: IntegerOrStringOrArray = serde_json::from_value(json).unwrap();
+        assert_eq!(integer, IntegerOrStringOrArray::Integer(42));
+
+        let json = json!("42");
+        let string: IntegerOrStringOrArray = serde_json::from_value(json).unwrap();
+        assert_eq!(string, IntegerOrStringOrArray::String("42".to_string()));
+
+        let json = json!([42, 432]);
+        let array: IntegerOrStringOrArray = serde_json::from_value(json).unwrap();
+        assert_eq!(
+            array,
+            IntegerOrStringOrArray::Array(vec![IntegerOrString::Integer(42), IntegerOrString::Integer(432)])
+        );
+
+        let json = json!(["42", "43"]);
+        let array: IntegerOrStringOrArray = serde_json::from_value(json).unwrap();
+        assert_eq!(
+            array,
+            IntegerOrStringOrArray::Array(vec![
+                IntegerOrString::String("42".to_string()),
+                IntegerOrString::String("43".to_string())
+            ])
+        );
+    }
 }
